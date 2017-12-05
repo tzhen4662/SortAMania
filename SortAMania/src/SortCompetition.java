7c4825166ff83ab13fb5c7562e0a95c06791c43f
Teacher <Teacher@10.8.39.80>
2017-12-05 12:05:56 -0500

    Added query parameter for challenge 2 and 5.

--- SortAMania/src/SortCompetition.java
+++ SortAMania/src/SortCompetition.java
@@ -4,13 +4,13 @@ public abstract class SortCompetition {
 	
 	public abstract int challengeOne(int[] arr);
 	
-	public abstract int challengeTwo(String[] arr);
+	public abstract int challengeTwo(String[] arr, String query);
 	
 	public abstract int challengeThree(int[] arr);
 	
 	public abstract int challengeFour(int[][] arr);
 	
-	public abstract int challengeFive(Object[] arr);
+	public abstract int challengeFive(Object[] arr, Object query);
 	
 	//Add a custom greeting so your sorter can introduce itself
 	public abstract String greeting();
