b445da1de5d431e8258d5ae849fce4b54c41f8b8
Teacher <Teacher@10.8.39.80>
2017-12-05 13:11:29 -0500

    Changed Object to Comparable objects, to eliminate the need for awkward casting.

--- SortAMania/src/SortCompetition.java
+++ SortAMania/src/SortCompetition.java
@@ -10,7 +10,7 @@ public abstract class SortCompetition {
 	
 	public abstract int challengeFour(int[][] arr);
 	
-	public abstract int challengeFive(Object[] arr, Object query);
+	public abstract int challengeFive(Comparable[] arr, Comparable query);
 	
 	//Add a custom greeting so your sorter can introduce itself
 	public abstract String greeting();
