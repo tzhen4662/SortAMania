84c17506d51cb7b0341c839193f7828fc3839d08
bruddanaton <levinnat88@gmail.com>
2017-12-05 11:46:39 -0500

    Initialized sortamania repository, students should add their entries and extend SortCopmetition

---
+++ SortAMania/src/SortCompetition.java
@@ -0,0 +1,18 @@
+
+public abstract class SortCompetition {
+	
+	
+	public abstract int challengeOne(int[] arr);
+	
+	public abstract int challengeTwo(String[] arr);
+	
+	public abstract int challengeThree(int[] arr);
+	
+	public abstract int challengeFour(int[][] arr);
+	
+	public abstract int challengeFive(Object[] arr);
+	
+	//Add a custom greeting so your sorter can introduce itself
+	public abstract String greeting();
+
+}
